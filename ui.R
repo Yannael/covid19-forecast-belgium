fa71776ac2409709aeb6ac0eb2d039733f1d4fc3
yannael <yannael@gmail.com>
2020-05-29 09:52:19 +0200

    Update help tab

--- ui.R
+++ ui.R
@@ -30,13 +30,9 @@ ui <- navbarPage(
   
   tabPanel("Help",
            h3("Explore tabs"),
-           h5("All: contains all of the processed data including those with missing required fields"),
-           h5("Latest: subset of `All` that only contains the most recent forecast for each team-model"),
-           h5("Latest targets: summarizes `Latest` to see which targets are included"),
-           h5("Latest locations: summarizes `Latest` to see which locations are included"),
-           h5("Latest quantiles: summarizes `Latest` to see which quantiles are included"),
-           h3("Usage"),
-           h4("Each table has the capability to be searched and filtered")
+           h5("Predictions: Visualise predictions for the different forecasting models"),
+           h5("Model accuracy comparison: Compares model prediction accuracy thanks to a pivot table"),
+           h5("All: contains all of the forecast data")
   )
 )
 
